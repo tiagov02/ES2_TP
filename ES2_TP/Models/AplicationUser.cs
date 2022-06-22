1133d4713c4b7e9cc786c4a0a8563db4b3575b80
Tiago Viana <tiagoviana.1003@gmail.com>
2022-06-22 15:20:07 +0100

    alteração

--- ES2_TP/Models/AplicationUser.cs
+++ ES2_TP/Models/AplicationUser.cs
@@ -3,5 +3,8 @@
     public class AplicationUser : Microsoft.AspNetCore.Identity.IdentityUser
     {
         public bool IsAdmin { get; set; }
+        public string nome { get; set; }
+        public string pais { get; set; }
+        public string telefone { get; set; }
     }
 }
--- ES2_TP/Models/Talento.cs
+++ ES2_TP/Models/Talento.cs
@@ -1,7 +1,7 @@
 ﻿namespace ES2_TP.Models
 {
     public class Talento
-    {
+    { 
         public Guid Id { get; set; }
         public float precoHora { get; set; }
         public float horasExperiencia { get; set; }
