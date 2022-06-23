f78cae5c5bf508ca91f5df2c1cebc389cd4e25ef
Tiago Viana <tiagoviana.1003@gmail.com>
2022-06-23 15:19:00 +0100

    .

---
+++ ES2_TP/Data/Migrations/20220623135711_002.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ES2_TP.Data.Migrations
+{
+    public partial class _002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Talento",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Talento");
+        }
+    }
+}
--- ES2_TP/Models/Talento.cs
+++ ES2_TP/Models/Talento.cs
@@ -7,5 +7,6 @@
         public float horasExperiencia { get; set; }
         public string nome { get; set; }
         public string pais { get; set; }
+        public string email { get; set; }
     }
 }
